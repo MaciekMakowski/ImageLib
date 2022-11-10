a9e068f3b398eb0afc73ba96cd05b5cbae0001ee
MaciekMakowski <73698286+MaciekMakowski@users.noreply.github.com>
2022-11-10 11:04:16 +0100

    Added Histogram plot

--- Lab2/Histogram.py
+++ Lab2/Histogram.py
@@ -1,4 +1,7 @@
-from BaseImage import np
+import matplotlib.pyplot as plt
+import numpy as np
+
+from BaseImage import *
 
 class _Histogram:
     """
@@ -7,10 +10,47 @@ class _Histogram:
     values: np.ndarray  # atrybut przechowujacy wartosci histogramu danego obrazu
 
     def __init__(self, values: np.ndarray) -> None:
+        if values.ndim == 2:
+            self.values = np.histogram(values, bins=256, range=(0, 256))[0]
+        else:
+            LR, LG, LB = np.squeeze(np.dsplit(values, values.shape[-1]))
+            LR = np.histogram(LR, bins=256, range=(0, 255))[0]
+            LG = np.histogram(LG, bins=256, range=(0, 255))[0]
+            LB = np.histogram(LB, bins=256, range=(0, 255))[0]
+            self.values = np.dstack((LR, LG, LB))
         pass
+
+    def get_layers(self):
+        return np.squeeze(np.dsplit(self.values, self.values.shape[-1]))
 
     def plot(self) -> None:
         """
         metoda wyswietlajaca histogram na podstawie atrybutu values
         """
+        if self.values.ndim > 2:
+            self.show_multi_histograms()
+        elif self.values.ndim:
+            plt.title('Gray')
+            plt.plot(np.linspace(0, 255, 256), self.values,color='black')
+        plt.show()
         pass
+
+    def show_multi_histograms(self):
+        plt.figure(figsize=(15, 6))
+        num = 1
+        space = np.linspace(0, 255, 256)
+        for layer in self.get_layers():
+            plt.subplot(1, 3, num)
+            plt.title(self.hist_color(num))
+            plt.xlim([0, 256])
+            plt.plot(space, layer, color=self.hist_color(num))
+            num += 1
+
+    def hist_color(self, num: int) -> str:
+        match num:
+            case 1:
+                return "red"
+            case 2:
+                return "green"
+            case 3:
+                return "blue"
