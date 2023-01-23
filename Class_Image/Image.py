318811c8cabcc77d6710d63742acbb76e4fc2756
MaciekMakowski <mvciek.mvkowski@gmail.com>
2023-01-23 18:11:43 +0100

    final version

--- Class_Image/Image.py
+++ Class_Image/Image.py
@@ -27,13 +27,13 @@ class Image(GrayScaleTransform, ImageComparison, ImageAligning, ImageFiltration,
             self.color_model = img.color_model
         else:
             img = self
-        theresh = Image(data=img.data, model=ColorModel.gray).otsu()
+        theresh = Image(data=img.data, model=ColorModel.gray).th_adaptive(17,8)
         canny = Image(data=theresh.data, model=ColorModel.gray).canny(20, 50, 3)
         lines = cv2.HoughLinesP(canny.data, 2, np.pi / 180, 30)
         result_lines_img = cv2.cvtColor(img.data, cv2.COLOR_GRAY2RGB)
         for line in lines:
             x0, y0, x1, y1 = line[0]
-            cv2.line(result_lines_img, (x0, y0), (x1, y1), (r, g, b), 2)
+            cv2.line(result_lines_img, (x0, y0), (x1, y1), (r, g, b), 8)
 
         self.data = result_lines_img
         return self
